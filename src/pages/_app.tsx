0fbf10b38424186c32927c51afd7c9037e358d56
Ryoji Kono <k.ryoji.74@gmail.com>
2023-04-13 22:42:54 +0900

    ✨ Apply startViewTransition on browser back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { Layout } from "@/components/layout";
+import { useTransitionRouterPush } from "@/hooks/useTransitionRouterPush";
 import "@/styles/globals.css";
 import "@/styles/reset.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { routerPushWithTransition } = useTransitionRouterPush();
+  const router = useRouter();
+
+  useEffect(() => {
+    router.beforePopState(({ as }) => {
+      routerPushWithTransition(as);
+      return false;
+    });
+  }, [router, routerPushWithTransition]);
+
   return (
     <Layout>
       <Component {...pageProps} />
